8efa26f40f9e21958633ef5c351141ac9e788229
paulaDevelopp <paulamartivas@gmail.com>
2025-04-26 20:56:32 +0200

    ❓ Añadir modelo de pregunta con respuestas (QuestionWithAnswers)
    - QuestionWithAnswers.kt:
      - Modelo que agrupa una pregunta de trivia junto a sus posibles respuestas.
      - Contiene:
        - id: Identificador único de la pregunta.
        - questionText: Texto de la pregunta.
        - correctAnswerId: ID de la respuesta correcta.
        - nswers: Lista de respuestas posibles (List<AnswerEntity>).
    
    - Anotado con @IgnoreExtraProperties para permitir flexibilidad extra al leer desde Firebase Realtime Database.
    - Incluye un constructor vacío obligatorio para la deserialización automática de Firebase.
    
    - Es usado tanto en partidas individuales como multijugador.

---
+++ app/src/main/java/com/example/otriviafan/data/model/QuestionWithAnswers.kt
@@ -0,0 +1,15 @@
+package com.example.otriviafan.data.model
+
+import com.example.otriviafan.data.entities.AnswerEntity
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+data class QuestionWithAnswers(
+    var id: Int = 0,
+    var questionText: String = "",
+    var correctAnswerId: Int = 0,
+    var answers: List<AnswerEntity> = emptyList()
+) {
+    // Constructor vacío requerido por Firebase
+    constructor() : this(0, "", 0, emptyList())
+}
